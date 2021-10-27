d995d8141f2dcdcef40d15a60d31acbf9fc11cb5
karlaaraujo <karlachs.a@gmail.com>
2021-10-27 10:56:39 -0300

    refactor: DHCP

--- dhcp/startup.sh
+++ dhcp/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 10.0.0.253" > /etc/resolv.conf
 ifconfig eth0 192.168.0.254
 apt-get update -y
 apt-get install -y isc-dhcp-server
